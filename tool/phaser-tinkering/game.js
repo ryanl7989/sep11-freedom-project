c13013e761cd99446874f6489d8d7d20a17f388b
Ryan <ryanl308@nycstudents.net>
2025-10-05 03:41:04 +0000

    Sat, Oct 4, 2025, 11:41 PM -04:00

--- tool/phaser-tinkering/game.js
+++ tool/phaser-tinkering/game.js
@@ -8,7 +8,7 @@ class Gamescene extends Phaser.Scene {
     }
 
     create() {
-        g.circle = this.add.circle(config.width/2,config.height/2,)
+        g.circle = this.add.circle(config.width/2,config.height/2,20,0xffffff)
     }
 
     update() {
