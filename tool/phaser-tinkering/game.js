442cc030c3dc4c57f99f9d803b6703bf7b16ee05
Ryan <ryanl308@nycstudents.net>
2025-10-05 03:19:33 +0000

    Sat, Oct 4, 2025, 11:19 PM -04:00

--- tool/phaser-tinkering/game.js
+++ tool/phaser-tinkering/game.js
@@ -1,19 +1,21 @@
 
-
+window.addEventListener('resize', () => {
+    window.location.reload()
+})
 
 const g = {}
 const config: {
-    type: Phaser.AUTO,
-    height: window.innerHeight,
-    width: window.innerWidth,
-    backgroundColor: '#123456',
+    type: Phaser.AUTO;
+    height: window.innerHeight;
+    width: window.innerWidth;
+    backgroundColor: '#123456';
     physics: {
-        default: 'arcade',
+        default: 'arcade';
         arcade: {
-            gravity: {y:0, x:0},
+            gravity: {y:0, x:0};
             debug: false
         }
-    },
+    };
     // scene: [GameScene]
 }
 
