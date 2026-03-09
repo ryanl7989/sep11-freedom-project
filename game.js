77cfaa4330d223f7f00e4be4aacc91556f4e3275
Ryan Lin <ryanl308@nycstudents.net>
2026-03-09 01:33:27 -0400

    project update

---
+++ game.js
@@ -0,0 +1,206 @@
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super('GameScene')
+    }
+
+    preload() {
+    // load assets
+        this.load.atlas('knight', 'assets/knightSpriteSheet.png', 'assets/knightSprites.json');
+        this.load.atlas('goblin', 'assets/goblinSpriteSheet.png', 'assets/goblinSprites.json');
+        this.load.atlas('slash', 'assets/slashSpriteSheet.png', 'assets/slashSprites.json');
+        this.load.image('oldDungeon', 'assets/oldDungeon.png');
+    }
+
+    create() {
+
+    // background
+        const backgroundImage = this.add.image(0, 0, 'oldDungeon');
+        backgroundImage.setOrigin(0.5, 0.5);
+        backgroundImage.setPosition(this.sys.game.config.width / 2, this.sys.game.config.height / 2);
+        const scaleX = this.sys.game.config.width / backgroundImage.width;
+        const scaleY = this.sys.game.config.height / backgroundImage.height;
+        const scale = Math.max(scaleX, scaleY);
+        backgroundImage.setScale(scale);
+
+    // animations for sprites
+        this.anims.create({ key:'kmoving', frames: this.anims.generateFrameNames('knight', {prefix:'knight', end: 6, zeroPad:1}), repeat: -1});
+        this.anims.create({ key:'kstand', frames: this.anims.generateFrameNames('knight', {prefix:'knightStand', end: 0, zeroPad:1}), repeat: -1});
+
+        this.anims.create({ key:'attack', frames: this.anims.generateFrameNames('slash', {prefix:'slash', end: 9, zeroPad:1}), repeat: 0});
+
+        this.anims.create({ key:'gmoving', frames: this.anims.generateFrameNames('goblin', {prefix:'goblin', end: 4, zeroPad:1}), repeat: -1});
+
+    // knight spawner
+        this.knight = this.physics.add.sprite(backgroundImage.width/2, backgroundImage.height/2, 'knight');
+        this.knight.setCollideWorldBounds(true);
+
+
+
+
+    // Goblin spawner
+
+
+        this.goblin = this.physics.add.sprite(10000, 10000, 'goblin');
+
+
+        this.goblinArray = []
+        this.time.addEvent({
+            delay: 4000,
+            callback: () => {
+                this.goblin = this.physics.add.sprite(Math.random()*(backgroundImage.width), Math.random()*(backgroundImage.height), 'goblin'); // creates the goblin at a random place
+                function destroyknight() {
+                    this.knight.destroy(); // if the knight hits a goblin the knight gets destroyed
+                }
+
+                this.physics.add.collider(this.goblin, this.knight, destroyknight, null, this); // adds collison between goblin and knight
+                this.goblinArray.push(this.goblin);
+                
+                for (let a = 0; a < this.goblinArray.length; a++){
+                    this.physics.add.collider(this.goblinArray[a], this.goblin); // adds collison for every goblin
+                }
+                this.goblin.setCollideWorldBounds(true); // stops the goblin from leaving the map
+                console.log(this.goblinArray);
+            },
+            loop: true
+        });
+
+
+    // create controls
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+    // game follows the knight
+        this.cameras.main.startFollow(this.knight);
+        // this.cameras.main.setZoom(2); // set zoom
+
+    // knight attack
+        var slashExist = false
+        this.input.on('pointerdown', function (pointer) {
+            this.slash = this.physics.add.sprite(this.knight.x, this.knight.y, 'slash'); // creates the slash sprite on the knight sprite
+            this.slash.scale=2.5    // makes the slash 3 times bigger than original
+            this.slash.anims.play('attack', true); // plays animations when there is a click
+            slashExist = true
+
+
+            for (let b = 0; b < this.goblinArray.length; b++){
+                const destroygoblin = (slash, goblin) => {
+                    goblin.destroy(); // if a goblin gets hit by a slash it gets destroyed
+                    var index = this.goblinArray.indexOf(goblin) // finds the index of the goblin that is hit by a slash
+                    this.goblinArray.splice(index, 1) // removes the goblin sprite from the goblin array
+                    console.log("hit")
+                }
+
+                if (this.slash) {
+                    this.physics.add.collider(this.slash, this.goblinArray[b], destroygoblin, null, this) // adds collision between the slash and all goblins
+                }
+            }
+
+            // 0.35 seconds after I click the attack animation will end and the slash will be deleted.
+            this.time.addEvent({
+                delay: 350,
+                callback: () => {
+                    this.slashExist = false
+                    this.slash.setVisible(false)
+                    this.slash.destroy()
+                },
+            });
+
+        }, this);
+
+    }
+
+    update () {
+
+
+
+    // Game Over
+        if (this.knight.active == false) {
+            alert("game over");
+            this.scene.stop('GameScene'); //stops the game when the knight dies
+            return; // ends the update
+        }
+
+    // makes goblins follow knight
+        for (let i = 0; i < this.goblinArray.length; i++) {
+            this.physics.moveToObject(this.goblinArray[i], this.knight, 100);
+
+
+    // goblin animation
+        }
+        if(this.goblinArray.length == 0){ // If no goblins exist, stop animations from playing
+        } else {
+            this.goblin.anims.play('gmoving', true);
+        }
+
+
+    // controls
+        this.knight.setVelocity(0,0);
+
+        if (this.cursors.left.isDown)
+        {
+            this.knight.setVelocityX(-150);
+            this.knight.anims.play('kmoving', true);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.knight.setVelocityX(150);
+            this.knight.anims.play('kmoving', true);
+        }
+
+
+        if (this.cursors.up.isDown)
+        {
+            this.knight.setVelocityY(-150);
+            this.knight.anims.play('kmoving', true);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.knight.setVelocityY(150);
+            this.knight.anims.play('kmoving', true);
+        }
+
+        if ((this.cursors.up.isDown == true &&  this.cursors.left.isDown == true)) {
+            this.knight.setVelocity(-Math.sqrt(11250),-Math.sqrt(11250));
+            this.knight.anims.play('kmoving', true);
+        }
+        if ((this.cursors.up.isDown == true &&  this.cursors.right.isDown == true)) {
+            this.knight.setVelocity(Math.sqrt(11250),-Math.sqrt(11250));
+            this.knight.anims.play('kmoving', true);
+        }
+        if ((this.cursors.down.isDown == true &&  this.cursors.left.isDown == true)) {
+            this.knight.setVelocity(-Math.sqrt(11250),Math.sqrt(11250));
+            this.knight.anims.play('kmoving', true);
+        }
+        if ((this.cursors.down.isDown == true &&  this.cursors.right.isDown == true)) {
+            this.knight.setVelocity(Math.sqrt(11250),Math.sqrt(11250));
+            this.knight.anims.play('kmoving', true);
+        }
+
+        if (this.cursors.left.isDown || this.cursors.right.isDown || this.cursors.up.isDown || this.cursors.down.isDown) {
+        } else {
+            this.knight.play('kstand')
+        }
+
+
+    }
+}
+// game configuration
+const config = {
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        height: 1040,
+        width: 1920,
+    },
+    backgroundColor: `#FFFFFF`,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y:0, x:0},
+            debug: false
+        }
+    },
+    scene: [ GameScene ]
+}
+
+const game = new Phaser.Game(config)
